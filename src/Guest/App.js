b11e1d7e02828cb71b295edcc253dd23c3f1fa6a
Abhijith <abhijithtp2003@gmail.com>
2024-05-03 13:55:43 +0530

    guest file name changed

--- src/Guest/App.js
+++ src/Guest/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import index from "./pages/index";
+import home from "./pages/home";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<index />} />
+      <Route path="/" element={<home />} />
     </Routes>
   );
 };
